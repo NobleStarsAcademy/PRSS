eeabd0d8007a057b5a156d20abc82317b34adcb4
NobleStarsAcademy <noblestarsacademyschool@gmail.com>
2026-03-27 11:10:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -149,10 +149,13 @@ form.addEventListener('submit', async (e) => {
         // Success - Show success message briefly then redirect
         showMessage('Access granted! Redirecting to your report...', 'success');
         
-        setTimeout(() => {
             // Open Google Drive link in new tab
-            window.open(pupilDatabase[key], '_blank');
-            setLoading(false);
+            const newTab = window.open('', '_blank'); // open immediately
+
+setTimeout(() => {
+    newTab.location.href = pupilDatabase[key];
+    setLoading(false);
+}, 1000);
             
             // Reset form after successful access
             setTimeout(() => {
